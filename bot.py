1408012d380b94082589df2077272ecaf41ebc85
zeyad3720 <zizoo372006@gmail.com>
2026-04-07 09:19:15 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8757102444:AAGKNHWN48c7_jyCbFt2_QthX1aAro2RyBU"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("أهلا بيك 👋 البوت شغال")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
